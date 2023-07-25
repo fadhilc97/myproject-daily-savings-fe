0e321b8db1be5e9520f85b2041b595a31b367bb5
Fadhil <fadhil@incit.org>
2023-07-25 21:45:10 +0700

    Setup react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import App from "./App.tsx";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
