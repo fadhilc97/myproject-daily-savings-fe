f9598c121db7feddcf968c08112c5283eba2eda6
Fadhil <fadhil@incit.org>
2023-08-03 21:35:25 +0700

    Input component, provide error message

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,4 @@
-import { Field } from "formik";
+import { Field, ErrorMessage } from "formik";
 
 type Props = {
   name: string;
@@ -10,13 +10,18 @@ function Input({
   label,
   ...inputProps
 }: Props & React.ComponentProps<"input">) {
+  const errorClasses = "text-danger text-xs font-semibold";
+
   return (
     <div className="mb-5">
-      <label htmlFor={name} className="block">
+      <label htmlFor={name} className="block font-semibold text-sm">
         {label}
       </label>
       {name ? (
-        <Field name={name} className="mt-1" {...inputProps} />
+        <>
+          <Field name={name} className="mt-1" {...inputProps} />
+          <ErrorMessage name={name} component="p" className={errorClasses} />
+        </>
       ) : (
         <input className="mt-1" {...inputProps} />
       )}
