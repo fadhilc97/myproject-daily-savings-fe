a7927ebeb70426e32535f77f41cbc2fc5fef8f54
Fadhil <fadhil@incit.org>
2023-08-05 13:57:04 +0700

    Implement overlay when creating context

--- src/hooks/savings/useCreateSavings.ts
+++ src/hooks/savings/useCreateSavings.ts
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import axios, { AxiosResponse } from "axios";
 import { useMutation } from "react-query";
 import { ISavingsSchema } from "../../@schemas/savings-schema";
+import { OverlayContext } from "../../contexts/OverlayProvider";
 
 function useCreateSavings() {
   const navigate = useNavigate();
+  const overlayContext = useContext(OverlayContext);
 
   async function createSavings(
     payload: ISavingsSchema
@@ -13,16 +16,23 @@ function useCreateSavings() {
     return res;
   }
 
-  return useMutation(createSavings, {
+  const createSavingsMutation = useMutation(createSavings, {
+    onMutate() {
+      overlayContext.showOverlay(true, "Creating savings data...");
+    },
     onSuccess(res) {
+      overlayContext.showOverlay(false);
       if (res.status === 201) {
         navigate("/");
       }
     },
     onError(error) {
+      overlayContext.showOverlay(false);
       console.log(error);
     },
   });
+
+  return createSavingsMutation;
 }
 
 export default useCreateSavings;
