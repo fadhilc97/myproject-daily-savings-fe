6a5e805752e03982fde3d3c8a30f1922bf7d309e
Fadhil <fadhil@incit.org>
2023-09-11 21:47:37 +0700

    Fix loading overlay should return JSX, not boolean

--- src/components/LoadingOverlay.tsx
+++ src/components/LoadingOverlay.tsx
@@ -6,14 +6,13 @@ type Props = {
 };
 
 function LoadingOverlay({ show, text = "Loading..." }: Props) {
-  return (
-    show &&
-    ReactDOM.createPortal(
+  return ReactDOM.createPortal(
+    show && (
       <div className="bg-slate-900 absolute flex items-center justify-center top-0 bottom-0 left-0 right-0 opacity-70">
         <p className="text-white font-medium">{text}</p>
-      </div>,
-      document.getElementById("overlay")!
-    )
+      </div>
+    ),
+    document.getElementById("overlay")!
   );
 }
 
