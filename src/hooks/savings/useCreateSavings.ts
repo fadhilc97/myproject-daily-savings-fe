8c2fad17e1d7ef87107e3c7cc55fdd989f2f43aa
Fadhil <fadhil@incit.org>
2023-10-22 21:48:31 +0700

    Refactor useCustomMutation with redirect url and private endpoint options

--- src/hooks/savings/useCreateSavings.ts
+++ src/hooks/savings/useCreateSavings.ts
@@ -1,40 +1,20 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { AxiosResponse } from "axios";
-import { useMutation } from "react-query";
 import { ISavingsSchema } from "../../@schemas/savings-schema";
-import { OverlayContext } from "../../contexts/OverlayProvider";
-import { AlertContext } from "../../contexts/AlertProvider";
-import axios from "../../@config/axios";
+import useCustomMutation from "../useCustomMutation";
 
 function useCreateSavings() {
-  const navigate = useNavigate();
-  const overlayContext = useContext(OverlayContext);
-  const alertContext = useContext(AlertContext);
-
-  async function createSavings(
-    payload: ISavingsSchema
-  ): Promise<AxiosResponse<IResponse>> {
-    const res = axios.post("/api/v1/savings", payload);
-    return res;
-  }
-
-  const createSavingsMutation = useMutation(createSavings, {
-    onMutate() {
-      overlayContext.showOverlay(true, "Memproses data...");
+  return useCustomMutation<ISavingsSchema>({
+    method: "POST",
+    url: "/api/v1/savings",
+    isPrivate: true,
+    messages: {
+      onMutate: "Memproses data...",
+      onSuccess: "Data berhasil ditambahkan",
+      onError: "Data gagal ditambahkan",
     },
-    onSuccess() {
-      overlayContext.showOverlay(false);
-      alertContext.showAlert("success", "Data berhasil ditambahkan");
-      navigate("/");
-    },
-    onError() {
-      overlayContext.showOverlay(false);
-      alertContext.showAlert("error", "Data gagal ditambahkan");
+    redirectUrl: {
+      onSuccess: "/",
     },
   });
-
-  return createSavingsMutation;
 }
 
 export default useCreateSavings;
--- src/hooks/useCustomMutation.ts
+++ src/hooks/useCustomMutation.ts
@@ -5,52 +5,64 @@ import { AxiosRequestConfig, AxiosResponse } from "axios";
 import { AlertContext } from "../contexts/AlertProvider";
 import { OverlayContext } from "../contexts/OverlayProvider";
 import axios from "../@config/axios";
+import useAxiosPrivate from "./useAxiosPrivate";
 
 type Options = {
+  isPrivate?: boolean;
   method: "POST" | "PUT";
   url: string;
-  messages: {
-    onMutate: string;
-    onSuccess: string;
-    onError: string;
+  messages?: {
+    onMutate?: string;
+    onSuccess?: string;
+    onError?: string;
+  };
+  redirectUrl?: {
+    onSuccess?: string;
+    onError?: string;
   };
   axiosRequestConfig?: AxiosRequestConfig;
-  // TODO: need create custom callback onMutate, onSuccess, onError
 };
 
 const useCustomMutation = <P = unknown, R = IResponse>({
+  isPrivate = false,
   method,
   url,
   messages,
   axiosRequestConfig,
+  redirectUrl,
 }: Options) => {
+  const axiosPrivate = useAxiosPrivate();
   const navigate = useNavigate();
   const overlayContext = useContext(OverlayContext);
   const alertContext = useContext(AlertContext);
 
   function mutationFn(payload: P): Promise<AxiosResponse<R>> {
-    const res = axios({
+    const axiosRequestOptions: AxiosRequestConfig = {
       method,
       url,
       data: payload,
       ...axiosRequestConfig,
-    });
+    };
+    const res = isPrivate
+      ? axiosPrivate(axiosRequestOptions)
+      : axios(axiosRequestOptions);
     return res;
   }
 
   const registerMutation = useMutation({
     mutationFn,
     onMutate() {
-      overlayContext.showOverlay(true, messages.onMutate);
+      overlayContext.showOverlay(true, messages?.onMutate);
     },
     onSuccess() {
       overlayContext.showOverlay(false);
-      alertContext.showAlert("success", messages.onSuccess);
-      navigate("/login");
+      alertContext.showAlert("success", messages?.onSuccess || "");
+      navigate(redirectUrl?.onSuccess || "");
     },
     onError() {
       overlayContext.showOverlay(false);
-      alertContext.showAlert("error", messages.onError);
+      alertContext.showAlert("error", messages?.onError || "");
+      navigate(redirectUrl?.onError || "");
     },
   });
 
